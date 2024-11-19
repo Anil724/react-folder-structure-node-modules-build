8f5c869aa9f77965e826488ff7160569c14cc3dc
Anil724 <anilkumarjinkala@gmail.com>
2024-11-19 17:14:29 +0530

    Add files via upload

---
+++ folder/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>In this session we learn difference b/w html and jsxml how to create build folder.</h1>
+       <img src="https://i.ytimg.com/vi/odACRQfasqs/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYACogWKAgwIABABGEAgWyhyMA8=&rs=AOn4CLBpLodoSf1leABIhw-jHIkixLsmuQ"></img>
+    </div>
+  );
+}
+
+export default App;
